792c61b8855795d6ea9f562588d15a64b4e02573
= <=>
2025-05-04 18:38:19 -0500

    Wrote bare bones user class (i dont want to do my presentation)

--- user.py
+++ user.py
@@ -1,6 +1,32 @@
-class UserClass:
+from threading import Thread
+from queue import Queue
+from server import FileServer
+
+class User:
     
-    def __init__(self, conn, server):
+    def __init__(self, conn, server:FileServer):
         self._conn = conn
         self._server = server
+        
+        self.activeRequest = None
+        self.sendQueue = Queue()
+        
+        self.sendThread = Thread(target = self.sendLoop)
+        self.sendThread.start()
+        
+        self.recvThread = Thread(target = self.recvLoop)
+        self.recvThread.start()
     
+    
+    def sendLoop(self):
+        while True:
+            if self.sendQueue[0]:
+                self._conn.send(self.sendQueue.pop().encode())
+    
+    
+    def recvLoop(self):
+        while True:
+            req = self._conn.recv(1024).decode()
+            response = self._server.handleRequest(req)
+            if response: 
+                self.sendQueue.put(response)
